953c80323555cf0d5530cb04c03fdcd43602d1b8
Ishani Herath <Ishani.Herath@dialog.lk>
2022-07-30 20:15:51 +0530

    comparison adding the rest of the page

--- src/comparison/Comparison.tsx
+++ src/comparison/Comparison.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Lens from '@material-ui/icons/Lens';
-
 import {
     Button,
     CardActionArea,
@@ -16,11 +15,30 @@ import {
 } from "@mui/material";
 import Box from "@mui/material/Box";
 import { brown, pink } from '@mui/material/colors';
-import { textAlign } from '@mui/system';
+import Looks5OutlinedIcon from '@material-ui/icons/Looks5Outlined';
+import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
 
 function Comparison() {
     const [itemOne, setItemOne] = React.useState<string>('');
     const [itemTwo, setItemTwo] = React.useState<string>('');
+    const options = [
+        {
+            label: "Lock 1",
+            value: "1",
+        },
+        {
+            label: "Lock 1",
+            value: "1",
+        },
+        {
+            label: "Lock 3",
+            value: "1",
+        },
+        {
+            label: "Lock 4",
+            value: "1",
+        },
+    ];
 
     const handleChangeItemOne = (event: SelectChangeEvent) => {
         setItemOne(event.target.value as string);
@@ -31,9 +49,9 @@ function Comparison() {
 
 
     return (
-        <Box>
+        <Box sx={{ py: 10 }}>
             <Box sx={{ textAlign: 'center' }}>
-                <Box sx={{ fontSize: '60px', fontWeight: 'bold', mt: '70px', mb: '15px' }}>
+                <Box sx={{ fontSize: '60px', fontWeight: 'bold', mb: '15px' }}>
                     Compare Door Lock Models
                 </Box>
                 <Box sx={{ fontSize: '20px' }}>
@@ -56,9 +74,9 @@ function Comparison() {
                             value={itemOne}
                             label="itemOne"
                             onChange={handleChangeItemOne}>
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
+                            {options.map((option) => (
+                                <MenuItem value={option.value}>{option.label}</MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
 
@@ -74,9 +92,8 @@ function Comparison() {
                         </React.Fragment>
                     </Box>
                     <Radio
+                        color="default"
                         checkedIcon={<Lens></Lens>}
-
-                        // {...controlProps('e')}
                         sx={{
                             color: pink[800],
                             '&.Mui-checked': {
@@ -91,31 +108,22 @@ function Comparison() {
                                 },
                             },
                             '& .MuiSvgIcon-root': {
-                                // color: pink[600],
-                                // fill: pink[600],
-                                fontSize: 50,
-                            },
-                            '& .MuiSvgIcon-colorSecondary': {
-                                // color: pink[600],
-                                // fill: green[600],
                                 fontSize: 50,
                             },
                         }}
                     />
                     <Radio
-                        // {...controlProps('e')}
                         sx={{
-                            color: brown[800],
-                            '&.Mui-checked': {
-                                color: brown[600],
-                            },
                             '& .MuiSvgIcon-root': {
                                 fontSize: 50,
+                            },
+                            root: {
+                                color: 'pink',
                             },
                         }}
                     />
 
-                    <Box sx={{ fontSize: '17px', borderTop: 1, borderColor: 'grey.400', textAlign: 'center', }}>
+                    <Box sx={{ fontSize: '17px', borderTop: 1, borderColor: 'grey.300', textAlign: 'center', }}>
                         <Box sx={{ fontSize: '17px', marginTop: '20px', fontWeight: 'bold' }}>
                             Starting at A$1,849
                         </Box>
@@ -135,9 +143,9 @@ function Comparison() {
                             value={itemTwo}
                             label="itemOne"
                             onChange={handleChangeItemTwo}>
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
+                            {options.map((option) => (
+                                <MenuItem value={option.value}>{option.label}</MenuItem>
+                            ))}
                         </Select>
                     </FormControl>
                     <Box sx={{ mt: 2 }}>
@@ -193,7 +201,7 @@ function Comparison() {
                             }}
                         />
 
-                        <Box sx={{ fontSize: '17px', borderTop: 1, borderColor: 'grey.400', textAlign: 'center', }}>
+                        <Box sx={{ fontSize: '17px', borderTop: 1, borderColor: 'grey.300', textAlign: 'center', }}>
                             <Box sx={{ fontSize: '17px', mt: '20px', fontWeight: 'bold' }}>
                                 Starting at A$1,849
                             </Box>
@@ -206,34 +214,107 @@ function Comparison() {
                 </Box>
             </Box >
 
-            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: '30px', p: '20px', borderBottom: 1, borderColor: 'grey.400' }}> Quick Look </Box>
+            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: 3, py: '20px', borderBottom: 1, borderColor: 'grey.300' }}> Quick Look </Box>
 
             <Box display={'flex'}>
                 <Box sx={{ width: '50%', height: '100%', p: 1, mx: 1, textAlign: 'center' }}>
-                    <Box sx={{ fontSize: '50px', fontWeight: 'bold' }}>
-                        6.7″
+                    <Box sx={{ m: 2 }}>
+                        <Typography sx={{ fontWeight: 'bold' }} variant="h2">
+                            6.7″
+                        </Typography>
+                        <Box sx={{ fontWeight: 'bold' }}>
+                            Super Retina XDR display with ProMotion <span style={{ verticalAlign: 'super' }}>1</span>
+                        </Box>
                     </Box>
-                    <Box sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                        Super Retina XDR display with ProMotion1
+                    <Box sx={{ m: 8 }}>
+                        <Looks5OutlinedIcon style={{ fontSize: 100 }} />
+                        <Box sx={{ fontWeight: 'bold' }}>Superfast 5G <span style={{ verticalAlign: 'super' }}>2</span>
+                        </Box>
+                    </Box>
+                    <Box sx={{ m: 8 }}>
+                        <CameraAltOutlinedIcon style={{ fontSize: 100 }} />
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Telephoto</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Wide</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Telephoto</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Cinematic mode in 1080p at 30 fps</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Dolby Vision HDR video recording up to 4K at 60 fps</Typography>
                     </Box>
                 </Box>
                 <Box sx={{ width: '50%', height: '100%', p: 1, mx: 1, textAlign: 'center' }}>
-                    <Box sx={{ fontSize: '50px', fontWeight: 'bold' }}>
-                        6.1″
+                    <Box sx={{ m: 2 }}>
+                        <Typography sx={{ fontWeight: 'bold' }} variant="h2">
+                            6.1″
+                        </Typography>
+                        <Box sx={{ fontWeight: 'bold' }}>
+                            Super Retina XDR display with ProMotion <span style={{ verticalAlign: 'super' }}>1</span>
+                        </Box>
                     </Box>
-                    <Box sx={{ fontWeight: 'bold', textAlign: 'center' }}>
-                        Super Retina XDR display1
+                    <Box sx={{ m: 8 }}>
+                        <Looks5OutlinedIcon style={{ fontSize: 100 }} />
+                        <Box sx={{ fontWeight: 'bold' }}>Superfast 5G <span style={{ verticalAlign: 'super' }}>2</span>
+                        </Box>
                     </Box>
+                    <Box sx={{ m: 8 }}>
+                        <CameraAltOutlinedIcon style={{ fontSize: 100 }} />
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Telephoto</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Wide</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Telephoto</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Cinematic mode in 1080p at 30 fps</Typography>
+                        <Typography sx={{ fontWeight: 'bold', my: 1 }}>Dolby Vision HDR video recording up to 4K at 60 fps</Typography>
+                    </Box>
+                </Box>
+            </Box >
+
+            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: 3, py: '20px', borderBottom: 1, borderColor: 'grey.300' }}> Size and Weight <span style={{ verticalAlign: 'super' }}>6</span> </Box>
+            <Box display={'flex'}>
+                <Box sx={{ width: '50%', height: '100%', p: 1, ml: 3, textAlign: 'left' }}>
+                    <Typography sx={{ fontWeight: 'bold' }} variant="subtitle1"> Height </Typography>
+                    <Typography> 160.8 mm (6.33 inches) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Width </Typography>
+                    <Typography> 78.1 mm (3.07 inches) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Depth </Typography>
+                    <Typography> 7.65 mm (0.30 inch) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Weight </Typography>
+                    <Typography> 238 grams (8.39 ounces) </Typography>
+                </Box>
+                <Box sx={{ width: '50%', height: '100%', p: 1, mx: 1, textAlign: 'left' }}>
+                    <Typography sx={{ fontWeight: 'bold' }} variant="subtitle1"> Height </Typography>
+                    <Typography> 160.8 mm (6.33 inches) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Width </Typography>
+                    <Typography> 78.1 mm (3.07 inches) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Depth </Typography>
+                    <Typography> 7.65 mm (0.30 inch) </Typography>
+                    <Typography sx={{ fontWeight: 'bold', mt: 3 }} variant="subtitle1"> Weight </Typography>
+                    <Typography> 238 grams (8.39 ounces) </Typography>
                 </Box>
             </Box>
 
-            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: '30px', p: '20px', borderBottom: 1, borderColor: 'grey.400' }}> Capacity </Box>
+            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: 3, py: '20px', borderBottom: 1, borderColor: 'grey.300' }}> Splash, Water and Dust Resistance <span style={{ verticalAlign: 'super' }}>4</span> </Box>
+            <Box display={'flex'}>
+                <Box sx={{ width: '50%', height: '100%', p: 1, mx: 3, textAlign: 'left' }}>
+                    <Typography> Rated IP68 (maximum depth of 6 metres for up to 30 minutes) under IEC standard 60529 (6.33 inches) </Typography>
+                </Box>
+                <Box sx={{ width: '50%', height: '100%', py: 1, textAlign: 'left' }}>
+                    <Typography> Rated IP68 (maximum depth of 6 metres for up to 30 minutes) under IEC standard 60529 </Typography>
+                </Box>
+            </Box>
 
-            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: '30px', p: '20px', borderBottom: 1, borderColor: 'grey.400' }}> Display </Box>
-
-            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: '30px', p: '20px', borderBottom: 1, borderColor: 'grey.400' }}> Size and Weight </Box>
-
-        </Box>
+            <Box sx={{ fontSize: '30px', fontWeight: 'bold', m: 3, py: '20px', borderBottom: 1, borderColor: 'grey.300' }}> Chip </Box>
+            <Box display={'flex'}>
+                <Box sx={{ width: '50%', height: '100%', p: 1, ml: 3, textAlign: 'left' }}>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> Height </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 6-core CPU with two performance cores and four efficiency cores </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 5-core GPU </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 16-core Neural Engine </Typography>
+                </Box>
+                <Box sx={{ width: '50%', height: '100%', p: 1, ml: 3, textAlign: 'left' }}>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> Height </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 6-core CPU with two performance cores and four efficiency cores </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 5-core GPU </Typography>
+                    <Typography sx={{ mt: 3 }} variant="subtitle1"> New 16-core Neural Engine </Typography>
+                </Box>
+            </Box>
+        </Box >
     );
 }
 
